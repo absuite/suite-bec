892348892521e5c549424ea5233074bebe3a62c3
pull user <pull user@163.com>
2017-10-02 11:10:19 +0800

    init

---
+++ database/preseeds/2017_00_08_100001_BecComponentSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Gmf\Sys\Builder;
+use Gmf\Sys\Models;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BecComponentSeeder extends Seeder {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$this->down();
+		$exception = DB::transaction(function () {
+			Models\Component::build(function (Builder $builder) {
+				$builder->code('becStatuteSetting')->name('信息定制');
+			});
+			Models\Component::build(function (Builder $builder) {
+				$builder->code('becStatuteNews')->name('最新信息');
+			});
+			Models\Component::build(function (Builder $builder) {
+				$builder->code('becStatuteQuery')->name('财税法规');
+			});
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Models\Component::where('code', 'like', 'bec.%')->delete();
+	}
+}
