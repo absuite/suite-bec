85ff7eff9327b477c1fbcf2b4b06089eff7417ae
pull user <pull user@163.com>
2017-10-31 23:03:28 +0800

    init

---
+++ database/preseeds/2017_00_08_880010_BecLnsItemPreSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Gmf\Sys\Builder;
+use Gmf\Sys\Models\LnsItem;
+use Illuminate\Database\Seeder;
+use Suite\Bec\Models\Post;
+
+class BecLnsItemPreSeeder extends Seeder {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		LnsItem::build(function (Builder $b) {
+			$b->type(Post::class)->code('bec')->name('环境数据量');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+
+	}
+}
