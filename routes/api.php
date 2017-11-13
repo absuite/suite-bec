cb0754181dbf1cd0820543c4c1e4500c7e0ab100
pull user <pull user@163.com>
2017-11-13 21:10:12 +0800

    init

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 $ns = 'Suite\Bec\Http\Controllers';
-Route::prefix('api/bec')->middleware(['api', 'auth:api', 'lns_check:bec'])->namespace($ns)->group(function () {
+Route::prefix('api/bec')->middleware(['api', 'auth:api'])->namespace($ns)->group(function () {
 	Route::resource('posts', 'PostController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 	Route::resource('prices', 'PriceController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 	Route::resource('tags', 'TagController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
